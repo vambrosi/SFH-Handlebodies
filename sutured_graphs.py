6828b047eadfa9a2a043455b801f5661f711c871
vambrosi <42151837+vambrosi@users.noreply.github.com>
2022-05-03 08:32:17 -0500

    Implemented SFH for all SuturedGraphs

--- sutured_graphs.py
+++ sutured_graphs.py
@@ -2,7 +2,7 @@
 Python implementation of sutured graphs. Those are labelled trivalent ribbon
 graphs. There are two possible labels for each vertex (+ or -) and edges are
 labelled by two integers: the first is nonnegative and even, and the second is
-odd if vertex labels differ and even if they are the same.
+odd if incident vertex labels differ and even if they are the same.
 
 Those graphs encode a sutured handlebody. The trivalent ribbon graph encodes a
 handlebody (by thickening it) together with a fixed parametrization of its
@@ -18,6 +18,12 @@ methods add_vertex or add_edge to build the graph.
 #-----------------------------------------------------------------------------#
 #  Basic Classes
 #-----------------------------------------------------------------------------#
+
+
+from typing import Optional
+from constructors import AAEdge, DDDVertex
+from modules import MultiModule
+from tensor import tensor
 
 
 class CyclicList(list):
@@ -95,6 +101,10 @@ class Edge(tuple):
     def twist(self):
         return tuple.__getitem__(self, 3)
 
+    def incident_to(self):
+        '''Returns vertices incident to the Edge.'''
+        return [self[0], self[1]]
+
 
 class Vertex(tuple):
     '''
@@ -147,9 +157,13 @@ class SuturedGraph:
         # cyclic list is the order given by the ribbon structure.
         self.incidence = dict()
 
-    def __getitem__(self, name):
+        # Initialize Sutured Floer ranks
+        self.SFH_rank = None
+        self.SFH_ranks = None
+
+    def __getitem__(self, name: str) -> Vertex:
         '''
-        Returns a vertex with name given (or None if there no such vertex).
+        Returns a vertex with name given.
         '''
         for vertex in self.vertices:
             if vertex.name == name:
@@ -157,11 +171,18 @@ class SuturedGraph:
 
         raise IndexError('No vertex with that name.')
 
+    def __call__(self, obj: str | Vertex | Edge, pos: int) -> \
+            tuple[Vertex | Edge, int]:
+        '''
+        Given a vertex/edge end, returns the matched edge/vertex end.
+        '''
+        return self.incident_to(obj)[pos]
+
     #-------------------------------------------------------------------------#
     # Graph-editing methods
     #-------------------------------------------------------------------------#
 
-    def add_vertex(self, name, sign):
+    def add_vertex(self, name: str, sign: str) -> None:
         '''
         Adds a vertex with the indicated name and label to the graph.
         '''
@@ -197,10 +218,10 @@ class SuturedGraph:
 
         # Unpacking variables (if needed)
         if isinstance(end1, str):
-            v_name = end1
+            v = end1
             v_insertion = None
         else:
-            v_name, v_insertion = end1
+            v, v_insertion = end1
         if isinstance(end2, str):
             w_name = end2
             w_insertion = None
@@ -210,7 +231,7 @@ class SuturedGraph:
         sutures, twist = labels
 
         # Vertices and edge references
-        v = self.get_vertex(v_name)
+        v = self.get_vertex(v)
         w = self.get_vertex(w_name)
         e = Edge(v, w, sutures, twist)
 
@@ -229,17 +250,34 @@ class SuturedGraph:
     # Local Properties
     #-------------------------------------------------------------------------#
 
-    def incident_to(self, v):
+    def incident_to(self, obj: str | Vertex | Edge) -> list[Edge | Vertex]:
         '''
-        Returns a list of edges incident to the given vertex. The order is
-        given by the ribbon structure on the graph.
+        Returns the list of edges/vertices incident to the given vertex/edge.
+        The order is given by the ribbon structure on the graph for vertices,
+        and by the implied direction for edges.
         '''
-        # In case v is just the vertex name and not the vertex
-        v = self.get_vertex(v)
+        if isinstance(obj, Vertex | str):
+            obj = self.get_vertex(obj)
 
-        return self.incidence[v]
+            result = []
+            for e in self.incidence[obj]:
+                if e[0] == obj:
+                    result.append((e, 0))
+                elif e[1] == obj:
+                    result.append((e, 1))
 
-    def get_vertex(self, v):
+            return result
+
+        elif isinstance(obj, Edge):
+            result = []
+
+            for v in obj.incident_to():
+                i = self.incidence[v].index(obj)
+                result.append((v, i))
+
+            return result
+
+    def get_vertex(self, v: Optional[str | Vertex]) -> Vertex:
         '''
         Function that normalizes vertex inputs. If the input is a vertex, it
         outputs it back, if it is a name it gives a vertex with that name back,
@@ -260,23 +298,16 @@ class SuturedGraph:
             # Only gets here if there are no vertices.
             raise IndexError('Graph is empty.')
 
-    def valence(self, v_name):
+    def valence(self, v: str | Vertex) -> int:
         '''
         Count the number of ends incident to the vertex. (It counts loops twice
         and edges with different ends once.) The function takes as input the
         name of the vertex or the vertex itself.
         '''
 
-        # Gets a vertex if given a vertex name
-        v = self.get_vertex(v_name)
+        return len(self.incident_to(v))
 
-        # Since there can be loops, we have to separate into two cases.
-        loops = [e for e in self.incident_to(v) if e[0] == e[1]]
-        open_edges = [e for e in self.incident_to(v) if e[0] != e[1]]
-
-        return 2*len(loops) + len(open_edges)
-
-    def check_valence(self):
+    def check_valence(self) -> bool:
         '''
         Checks if the graph is trivalent. This is necessary to compute SFH of
         the associated sutured handlebody.
@@ -284,11 +315,63 @@ class SuturedGraph:
 
         return all(self.valence(v) == 3 for v in self.vertices)
 
+    def BS(self, a: str | Vertex | Edge) -> MultiModule:
+        '''
+        Returns the bordered sutured modules associated with a vertex or edge.
+        '''
+
+        if isinstance(a, str | Vertex):
+            a = self.get_vertex(a)
+
+            assert self.valence(a) == 3, 'Vertex must have valence 3.'
+            e1, e2, e3 = self.incidence[a]
+
+            M = DDDVertex(e1.sutures, e2.sutures, e3.sutures)
+            return M if a.label == '+' else M.mirror()
+
+        elif isinstance(a, Edge):
+            return AAEdge(a.sutures, a.twist, a[0].label)
+
     #-------------------------------------------------------------------------#
     # Global Properties
     #-------------------------------------------------------------------------#
 
-    def spanning_tree(self, root_name=None):
+    def connected_components(self) -> set[set[Vertex]]:
+        '''
+        Returns the connected components as sets of vertices.
+        '''
+        components = set()
+
+        # If one end of an edge is in a component, add both of its ends to it.
+        # If the edge is not incident to any component, add a new component.
+        for e in self.edges:
+            for component in components:
+                if e[0] in component or e[1] in component:
+                    component.add(e[0])
+                    component.add(e[1])
+                    break
+            else:
+                components.add({e[0], e[1]})
+
+        return components
+
+    def number_components(self) -> int:
+        return len(self.number_components())
+
+    def is_connected(self) -> bool:
+        return self.number_components() == 1
+
+    def genus(self) -> int:
+        '''
+        Computes the genus of the graph.
+        '''
+
+        # Euler characteristic = number of components - genus.
+        g = self.number_components() - len(self.vertices) + len(self.edges)
+
+        return g
+
+    def spanning_tree(self, root_name: str | Vertex = None) -> list[Edge]:
         '''
         Finds a spanning tree for the connected component of the vertex given.
         Input can be a vertex or a vertex name. It returns the set of edges in
@@ -318,7 +401,8 @@ class SuturedGraph:
 
         return spanning_tree
 
-    def fundamental_group_gens(self, root_name=None):
+    def fundamental_group_gens(self, root_name: str | Vertex = None) \
+            -> list[Edge]:
         '''
         Chooses a spanning tree and returns the edges complementary to it.
         If the graph is connected those edges represent the generators of the
@@ -332,16 +416,103 @@ class SuturedGraph:
         assert len(gens) == self.genus(), "Graph is not connected."
         return list(gens)
 
-    def genus(self):
-        '''
-        Computes the genus of the graph. Assumes the graph is connected.
-        '''
-        return 1 - len(self.vertices) + len(self.edges)
+    #-------------------------------------------------------------------------#
+    # Sutured Floer Invariants
+    #-------------------------------------------------------------------------#
 
-    def SFH_ranks(self, root_name=None):
+    @property
+    def SFH_rank(self):
+        '''Total rank of SFH of the Genus2Handlebody.'''
+        return self.SFH()[0] if self._SFH_rank == None else self._SFH_rank
+
+    @property
+    def SFH_ranks(self):
+        '''A dictionary that has as keys the spin_c gradings with nontrivial
+        sutured Floer homology. The value associated with a key is the rank
+        of SFH on the corresponding grading (with Z/2 coefficients).'''
+        return self.SFH()[1] if self._SFH_ranks == None else self._SFH_ranks
+
+    @SFH_rank.setter
+    def SFH_rank(self, value):
+        self._SFH_rank = value
+
+    @SFH_ranks.setter
+    def SFH_ranks(self, value):
+        self._SFH_ranks = value
+
+    def SFH(self, root_name: str | Vertex = None) -> MultiModule:
         '''
         Returns a dictionary {Spin^c grading: rank of SFH}.
         Assumes the sutured graph is connected.
         '''
         # Checks that graph is trivalent
         assert self.check_valence(), 'Graph is not trivalent.'
+
+        # Get the root or pick a root if one was not choosen
+        root = self.get_vertex(root_name)
+
+        # Computes largest bordered invariant before having to take HHs.
+        # In other words, it traverses a "graph" G such that all the vertices
+        # and edges of self are included but some edges are not attached to
+        # both of their ends. The result is required to be contractible. This
+        # graph G has an associated bordered invariant that is computed in
+        # the process (it is the M_left).
+
+        # Initialize variables
+        not_visited = self.vertices.union(self.edges)
+
+        not_visited.remove(root)
+        M_left = self.BS(root)
+        left_slots = self.incident_to(root)
+
+        while not_visited:
+            # Pop the first vertex or edge that is not visited
+            # Guarantees that we are not adding loops
+            for i, (obj, pos) in enumerate(left_slots):
+                if obj in not_visited:
+                    left_index = i
+                    left_obj, left_pos = obj, pos
+                    left_slots.pop(i)
+                    break
+
+            M_right = self.BS(left_obj)
+            right_slots = self.incident_to(left_obj)
+            not_visited.remove(left_obj)
+
+            # Pop the vertex or edge that is matched to left_slot
+            for j, (obj, pos) in enumerate(right_slots):
+                if self.incident_to(obj)[pos] == (left_obj, left_pos):
+                    right_index = j
+                    right_slots.pop(j)
+                    break
+
+            # Computes the bordered invariant by gluing left_slot
+            # to the corresponing slot on the right.
+            M_left = tensor(M_left, left_index, M_right, right_index)
+            left_slots = left_slots + right_slots
+
+        N = M_left
+
+        # Identifies matched pair and apply HH to them.
+        number_of_pairs = len(left_slots) // 2
+        while left_slots:
+            slot2 = left_slots.pop()
+            slot2_index = len(left_slots)
+
+            slot1_index = left_slots.index(self(*slot2))
+            left_slots.pop(slot1_index)
+
+            N = N.HH(slot1_index, slot2_index)
+
+        SFH_ranks = {}
+        for gen in N.generators:
+            point = M_left.count_chords(gen)[number_of_pairs::]
+            try:
+                SFH_ranks[point] += 1
+            except KeyError:
+                SFH_ranks[point] = 1
+
+        self.SFH_ranks = SFH_ranks
+        self.SFH_rank = len(N.generators)
+
+        return self.SFH_rank, self.SFH_ranks
